a4d64ce4b060d60205a3c26981798e607c6ec113
sravani1994 <lgolla@pip-llc.com>
2020-11-08 01:59:16 +0530

    FirstCommit

---
+++ src/testCases/TC_001.java
@@ -0,0 +1,19 @@
+package testCases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test
+	public void testcase1()
+	{
+		//System.setProperty("wedbriver.chrome.driver","./Driver/chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElementById("email").sendKeys("hello");
+		driver.findElementById("pass").sendKeys("hello");
+		driver.quit();
+	}
+
+}
